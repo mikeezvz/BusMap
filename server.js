f81b6b04e44cc59945264c4066c9b165f1840a5e
mikeezvz <mike.zogheib@outlook.com>
2025-10-26 12:19:01 +0100

    Setup environment for API

---
+++ server.js
@@ -0,0 +1,31 @@
+// server.js
+require("dotenv").config();
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+const ORS_API_KEY = process.env.ORS_API_KEY
+
+app.post("/route", async (req, res) => {
+  try {
+    const response = await fetch("https://api.openrouteservice.org/v2/directions/driving-car/geojson", {
+      method: "POST",
+      headers: {
+        "Authorization": ORS_API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ coordinates: req.body.coordinates }),
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.toString() });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server läuft auf http://localhost:${PORT}`));
